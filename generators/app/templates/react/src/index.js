13461d7d20bff8d7ba04ca0f9053866ca73482e6
Nathan Hoad <nathan@nathanhoad.net>
2017-11-07 13:48:58 +1000

    Added notice about webpack modules bug

--- generators/app/templates/react/src/index.js
+++ generators/app/templates/react/src/index.js
@@ -1,7 +1,7 @@
 const React = require('react');
 const { render } = require('react-dom');
 
-const PROJECT_NAME = '<%= projectName %>';
+const PROJECT_NAME = '<%= projectSlug %>';
 const root = document.querySelector(`[data-${PROJECT_NAME}-root]`);
 
 function init() {
